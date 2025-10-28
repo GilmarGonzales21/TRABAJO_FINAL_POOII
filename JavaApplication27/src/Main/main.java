585d9079d098ba88bb4f9fb0d3cab8e7c10b1ce2
GilmarGonzales21 <gilmarjoegonzaleshernandez5@gmail.com>
2025-10-27 23:11:17 -0500

    main

--- JavaApplication27/src/Main/main.java
+++ JavaApplication27/src/Main/main.java
@@ -14,7 +14,12 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+            
+
+    public static void main(String[] args) {
+        HomeController app = new HomeController();
+        app.run();
     }
-    
+}
+
 }
