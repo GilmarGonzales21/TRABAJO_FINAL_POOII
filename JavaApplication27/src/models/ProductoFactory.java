66fc0ac580f36fc1862476685c4f85bf301b28b2
GilmarGonzales21 <gilmarjoegonzaleshernandez5@gmail.com>
2025-11-24 09:02:00 -0500

    corrreciones de marcas

--- JavaApplication27/src/models/ProductoFactory.java
+++ JavaApplication27/src/models/ProductoFactory.java
@@ -17,15 +17,15 @@ public class ProductoFactory {
         // ===== Cemento =====
         catalogo.add(new Producto(
             "Cemento Sol", "Sol", "Cemento",
-            "Bolsa 42.5kg uso estructural", 28.50, 120
+            "Bolsa 42.5kg uso estructural", 30.00, 120
         ));
         catalogo.add(new Producto(
             "Cemento Andino", "Andino", "Cemento",
-            "Bolsa 42.5kg obra general", 27.90, 90
+            "Bolsa 42.5kg obra general", 29.50, 90
         ));
         catalogo.add(new Producto(
             "Cemento APU", "APU", "Cemento",
-            "Bolsa 42.5kg alta resistencia", 29.50, 60
+            "Bolsa 42.5kg alta resistencia", 27.50, 60
         ));
         catalogo.add(new Producto(
             "Cemento Holcim", "Holcim", "Cemento",
@@ -34,63 +34,63 @@ public class ProductoFactory {
 
         // ===== Tuberías =====
         catalogo.add(new Producto(
-            "Tubo PVC 4\"", "Tigre", "Tuberías",
+            "Tubo PVC 4\"", "Nicoll", "Tuberías",
             "Tubería PVC 4 pulgadas desagüe", 45.00, 40
         ));
         catalogo.add(new Producto(
-            "Tubo PVC 3\"", "Tigre", "Tuberías",
+            "Tubo PVC 3\"", "Nicoll", "Tuberías",
             "Tubería PVC 3 pulgadas desagüe", 32.00, 55
         ));
         catalogo.add(new Producto(
-            "Tubo PVC 2\"", "Tigre", "Tuberías",
+            "Tubo PVC 2\"", "Nicoll", "Tuberías",
             "Tubería PVC 2 pulgadas agua", 18.00, 80
         ));
         catalogo.add(new Producto(
-            "Tubo PVC 1\"", "Tigre", "Tuberías",
+            "Tubo PVC 1\"", "Nicoll", "Tuberías",
             "Tubería PVC 1 pulgada agua", 9.50, 100
         ));
         catalogo.add(new Producto(
-            "Tubo PVC 3/4\"", "Tigre", "Tuberías",
+            "Tubo PVC 3/4\"", "Nicoll", "Tuberías",
             "Tubería PVC 3/4 pulgada agua", 6.90, 150
         ));
 
         // ===== Conexiones =====
         catalogo.add(new Producto(
-            "Codo PVC 1/2\"", "Tigre", "Conexiones",
+            "Codo PVC 1/2\"", "Nicoll", "Conexiones",
             "Codo de 90° PVC 1/2 pulgada", 1.20, 300
         ));
         catalogo.add(new Producto(
-            "Codo PVC 2\"", "Tigre", "Conexiones",
+            "Codo PVC 2\"", "Nicoll", "Conexiones",
             "Codo de 90° PVC 2 pulgadas", 4.50, 120
         ));
         catalogo.add(new Producto(
-            "Codo PVC 4\"", "Tigre", "Conexiones",
+            "Codo PVC 4\"", "Nicoll", "Conexiones",
             "Codo de 90° PVC 4 pulgadas", 8.90, 60
         ));
 
         catalogo.add(new Producto(
-            "Tee PVC 1/2\"", "Tigre", "Conexiones",
+            "Tee PVC 1/2\"", "Nicoll", "Conexiones",
             "Tee PVC 1/2 pulgada", 1.50, 250
         ));
         catalogo.add(new Producto(
-            "Tee PVC 2\"", "Tigre", "Conexiones",
+            "Tee PVC 2\"", "Nicoll", "Conexiones",
             "Tee PVC 2 pulgadas", 4.90, 130
         ));
         catalogo.add(new Producto(
-            "Tee PVC 4\"", "Tigre", "Conexiones",
+            "Tee PVC 4\"", "Nicoll", "Conexiones",
             "Tee PVC 4 pulgadas", 9.50, 70
         ));
 
         catalogo.add(new Producto(
-            "Yee PVC 1/2\"", "Tigre", "Conexiones",
+            "Yee PVC 1/2\"", "Nicoll", "Conexiones",
             "Yee PVC 1/2 pulgada", 1.80, 200
         ));
         catalogo.add(new Producto(
-            "Yee PVC 2\"", "Tigre", "Conexiones",
+            "Yee PVC 2\"", "Nicoll", "Conexiones",
             "Yee PVC 2 pulgadas", 5.20, 110
         ));
         catalogo.add(new Producto(
-            "Yee PVC 4\"", "Tigre", "Conexiones",
+            "Yee PVC 4\"", "Nicoll", "Conexiones",
             "Yee PVC 4 pulgadas", 10.90, 65
         ));
 
