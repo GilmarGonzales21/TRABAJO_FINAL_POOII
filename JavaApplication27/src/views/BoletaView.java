42946e0b06a30302535adf872f3361827e995976
M1G31L <miguelpapibbb09@gmail.com>
2025-11-24 20:00:08 -0500

    color BoletaView

--- JavaApplication27/src/views/BoletaView.java
+++ JavaApplication27/src/views/BoletaView.java
@@ -9,21 +9,36 @@ import controllers.VentaController;
 import controllers.StaticAccess;
 import core.View;
 import core.Model;
-
+import java.awt.*;
 /**
  *
  * @author Gilmar Gonzales
  */
+@SuppressWarnings("serial")
 public class BoletaView extends JPanel implements View {
-
+    private final Color AMARILLO = new Color(247, 212, 0);
+    private final Color AZUL = new Color(26, 34, 56);
     private JTextArea txtBoleta;
-
+    private Image logo;
+    
     public BoletaView(VentaController ventaController) {
         setLayout(null);
-
+        try {
+            logo = new ImageIcon(getClass().getResource("/assets/logo.png")).getImage();
+        } catch (Exception e) {
+            System.out.println("ERROR cargando logo: " + e.getMessage());
+        }
         JLabel lbl = new JLabel("Boleta Emitida");
+         lbl.setFont(new Font("Segoe UI", Font.BOLD, 24));
+        lbl.setForeground(AZUL);
         lbl.setBounds(20, 10, 200, 20);
         add(lbl);
+         if (logo != null) {
+            Image small = logo.getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+            JLabel logoTitulo = new JLabel(new ImageIcon(small));
+            logoTitulo.setBounds(250, 8, 40, 40);
+            add(logoTitulo);
+        }
 
         txtBoleta = new JTextArea();
         JScrollPane scroll = new JScrollPane(txtBoleta);
@@ -41,9 +56,21 @@ public class BoletaView extends JPanel implements View {
         btnVolver.addActionListener(e -> {
             core.Controller.loadView("HomeView");
         });
+        btnActualizar.addActionListener(e -> {
+            if (ventaController.getBoletaActual() != null) {
+                txtBoleta.setText(ventaController.getBoletaActual().toString());
+            } else {
+                txtBoleta.setText("No hay boleta emitida aún.");
+            }
+        });
          cargarBoleta();
     }
-
+        @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(AMARILLO);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
         private void cargarBoleta() {
             VentaController vc = StaticAccess.venta();
             if (vc.getBoletaActual() != null) {
@@ -52,8 +79,6 @@ public class BoletaView extends JPanel implements View {
                 txtBoleta.setText("No hay boleta emitida aún.");
             }
         }
-    
-
     @Override
     public void update(Model model, Object data) {
          cargarBoleta();
